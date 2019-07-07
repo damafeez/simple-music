bf7aaa7e9c61a2b1ae9fe8dc4ca31ef9ae242c1b
Afeez Awoyemi <damafeez@gmail.com>
2019-07-07 22:18:42 +0100

    songs

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -5,3 +5,4 @@ const Color primary = Color(0xff070607);
 const Color primaryText = Color(0xff070607);
 const Color secondary = Color(0xffE7E7E7);
 const Color secondaryText = Color(0xff717171);
+const Color accentText = Color(0xff414A53);
